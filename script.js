6bfbceebfffd9a8662c994ab3d05b4d587a1c319
Piyush7408407994 <piyushmishra7408407994@gmail.com>
2025-07-30 20:41:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+	 const result = [];
+  let current = [];
+  let currentSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (currentSum + arr[i] <= n) {
+      current.push(arr[i]);
+      currentSum += arr[i];
+    } else {
+      result.push(current);
+      current = [arr[i]];
+      currentSum = arr[i];
+    }
+  }
+
+  // Push the last group if it's not empty
+  if (current.length > 0) {
+    result.push(current);
+  }
+
+  return result;
 };
 
 const n = prompt("Enter n: ");
